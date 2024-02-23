b9d3410e3bb1184466fe5501b3626154a4c12cce
vikrantsingh22 <vikrantsingh2020004@gmail.com>
2024-02-23 17:01:42 +0530

    Initial commit: Initialize app, add .gitignore, and create apiList.js

---
+++ util/apiList.js
@@ -0,0 +1,10 @@
+// List of the required API's for the application
+
+const apiList = `https://api.coingecko.com/api/v3/coins/list`;
+const convertCoin = (fromCurrency, date) =>
+  `https://api.coingecko.com/api/v3/coins/${fromCurrency}/history?date=${date}&localization=false`;
+
+const companyHoldings = (currency) =>
+  `https://api.coingecko.com/api/v3/companies/public_treasury/${currency}`;
+
+export { apiList, convertCoin, companyHoldings };
